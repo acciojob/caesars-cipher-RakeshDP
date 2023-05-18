be5c1fbd65915251917b0bd459d54a397be8b389
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-05-18 22:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ const lookup = {
 
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+	 // Only change code below this line
+
 	for( let i=0; i< encodedStr.length;i++){
 		if(lookup[encodedStr[i]] === undefined){
 			decodedArr.push(encodedStr[i]);
